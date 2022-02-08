2bd8884e25a0f25c0d061ad766be7e86105793ea
Ashwesh09 <ashwesh.chn@gmail.com>
2022-02-08 15:34:03 +0530

    Changes made

--- PrimitiveDatatypes.java
+++ PrimitiveDatatypes.java
@@ -1,10 +1,16 @@
 class PrimitiveDatatypes {
     public static void main(String[] args) {
+
         int intgr = 10;
+        long lng = 10000232;
+        byte bytes = 0;
+        short shrt = -15;
+        float flt = 2.5f;
         double decimal = 10.101;
         char character = 't';
         String string = "ten";
-        System.out.println("When datatypes were initialized:");
-        System.out.println("Interger = " + intgr + "\nFloat=" + decimal + "\nChar=" + character + "\nString=" + string);
+        boolean fls = true;
+
+        System.out.println("Interger=" + intgr +"\nLong="+lng+"\nBytes=" + bytes +"\nShort=" + shrt +"\nFloat=" + flt + "\nFloat=" + decimal + "\nChar=" + character + "\nString=" + string+"\nBoolean="+fls);
     }
 }
