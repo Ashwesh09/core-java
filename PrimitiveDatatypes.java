8a1ab4f0c90fd8a6e132a3be3b039cd52b45a5e2
Ashwesh09 <ashwesh.chn@gmail.com>
2022-02-08 14:29:39 +0530

    PrimitiveDatatypes Example

---
+++ PrimitiveDatatypes.java
@@ -0,0 +1,10 @@
+class PrimitiveDatatypes {
+    public static void main(String[] args) {
+        int intgr = 10;
+        double decimal = 10.101;
+        char character = 't';
+        String string = "ten";
+        System.out.println("When datatypes were initialized:");
+        System.out.println("Interger = " + intgr + "\nFloat=" + decimal + "\nChar=" + character + "\nString=" + string);
+    }
+}
