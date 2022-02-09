c5f967d8e1d3d97b19fe62e77604a9b5af87689e
Ashwesh09 <ashwesh.chn@gmail.com>
2022-02-09 13:14:16 +0530

    Simple operators use

---
+++ NumberGame2.java
@@ -0,0 +1,7 @@
+public class NumberGame2 {
+    public static void main(String[] args){
+        int a = 55,b = 70;
+        String res = (a<50 || a<b)?"Yes atleast one is true.":"No false!";
+        System.out.println(res);
+    }
+}
